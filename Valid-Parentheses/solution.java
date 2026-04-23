fc027e42ba30ab406e8ba663c214440de4d12f50
Pranav <113507394+prrrrnav@users.noreply.github.com>
2026-04-23 13:47:39 +0530

    Update: Valid Parentheses (java)

--- Valid-Parentheses/solution.java
+++ Valid-Parentheses/solution.java
@@ -1,24 +1,15 @@
-            stack.push(')');
-        }
-    return stack.isEmpty();
-        else if(item == '{'){
-            stack.push('}');
-        }
-        else if(item == '['){
-            stack.push(']');
-        }
-
-        if(item == '('){
-        else{
-            if(stack.isEmpty() || stack.pop()!=item){
-        }
-                return false;
-            }
-    }
-       for(char item : s.toCharArray()){
-
-        Deque<Character> stack = new ArrayDeque<>();
-       }
-}
-class Solution {
-    public boolean isValid(String s) {
+"()"
+"()[]{}"
+"(]"
+"([])"
+"([)]"
+true
+true
+false
+true
+false
+true
+true
+false
+true
+false
