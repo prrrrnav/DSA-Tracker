6b71b616bfc03021d8709b57690a5980c8530957
Pranav <113507394+prrrrnav@users.noreply.github.com>
2026-04-23 13:56:49 +0530

    Update: Contains Duplicate (java)

--- Contains-Duplicate/solution.java
+++ Contains-Duplicate/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for(int i:nums){
-            if(set)
+            if(set.contains(i))
         }
     }
             set.put(i);
