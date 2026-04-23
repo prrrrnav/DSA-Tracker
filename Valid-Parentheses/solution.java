f3d8ffa57cfe62b4eaa55ebc4b5f5010cc926ea5
Pranav <113507394+prrrrnav@users.noreply.github.com>
2026-04-23 13:45:36 +0530

    Update: Valid Parentheses (java)

--- Valid-Parentheses/solution.java
+++ Valid-Parentheses/solution.java
@@ -11,11 +11,14 @@
 
     
     return true;
-    }
-}
     public boolean isValid(String s) {
         Deque<char> stack = new ArrayDeque<>();
 
        for(char item : s.toCharArray()){
         if(item == '('){
 class Solution {
+        else{
+            
+        }
+    }
+}
