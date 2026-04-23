7c366fd0296dac23074b142d71d7c72350cac7f8
Pranav <113507394+prrrrnav@users.noreply.github.com>
2026-04-23 13:56:59 +0530

    Update: Contains Duplicate (java)

--- Contains-Duplicate/solution.java
+++ Contains-Duplicate/solution.java
@@ -3,7 +3,7 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         for(int i:nums){
             if(set.contains(i)){
-                return 
+                return fasle
             }
             set.put(i);
         }
