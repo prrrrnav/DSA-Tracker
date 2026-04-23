7d53634b4a2ef41b0e3ebf8348721f50c56d1904
Pranav <113507394+prrrrnav@users.noreply.github.com>
2026-04-23 12:55:27 +0530

    Add: Best Time to Buy and Sell Stock (java)

---
+++ Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        
+            int needed =  target - nums[i];
+        
+            map.put(nums[i],i);
+
+        }
+    return new int[] {1,1};
+                return new int[] {map.get(needed),i};
+            };
+    }
+
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i =0; i<nums.length;i++){
+            if(map.containsKey(needed)){
+}
